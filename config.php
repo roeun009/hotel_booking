a7725db7331dd6200eac68be49a9cf8242a462c0
Meun Chamroeun <meunchamroeun09@gmail.com>
2025-10-16 22:02:58 +0700

    add

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "sql301.infinityfree.com"; // check your InfinityFree DB hostname
+$username = "if0_40180102";
+$password = "yourpassword";
+$database = "if0_40180102_hotelcita";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully!";
+?>
